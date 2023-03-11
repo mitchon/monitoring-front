b36622c4594b57b863ec8de2a689c89eab167f34
Komarov Dmitriy <KomMitAl@yandex.ru>
2023-03-11 12:38:40 +0300

    added ws and api to receive from back

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Map from "./Map";
 
 function App() {
+
     return (
         <div className="App" style={{padding: '50px'}}>
             <Map></Map>
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useRef, useState} from 'react';
-import Leaflet, {LatLng} from 'leaflet';
-import {MapContainer, TileLayer, Marker, Popup, useMapEvents} from "react-leaflet";
+import Leaflet, {LatLng, LatLngBounds} from 'leaflet';
+import {MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap} from "react-leaflet";
 import CSS from "csstype";
 import 'leaflet/dist/leaflet.css';
+import useWebSocket from "react-use-websocket";
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import axios, {AxiosResponse} from "axios";
 
 let DefaultIcon = Leaflet.icon({
     iconUrl: icon,
@@ -34,13 +36,42 @@ function Markers() {
     )
 }
 
+const api: String | undefined = process.env.REACT_APP_API_URL
+
 function Map() {
 
     const position = new Leaflet.LatLng(55.752, 37.617)
     const mapStyle: CSS.Properties = {width: '100%', height: '90vh' }
+    const [map, setMap] = useState<Leaflet.Map | null>(null)
+
+    useWebSocket("ws://localhost:8080/socket", {
+        onOpen: () => console.log('opened'),
+        onClose: () => console.log('closed'),
+        onMessage(e: MessageEvent<string>) { console.log(e.data) },
+        retryOnError: true
+    })
+
+    const GetGeom = () => {
+        let newBounds = map ? map.getBounds() : null
+        console.log(newBounds)
+        console.log(process.env)
+        if (newBounds !== null) {
+            axios.get(api + "/graph/geom", {
+                params: {bounds: btoa(JSON.stringify(newBounds))}
+            }).then((response: AxiosResponse) => {
+                console.log(response.data)
+            })
+        }
+    }
+
     return (
         <div>
-            <MapContainer center={position} zoom={13} style={mapStyle}>
+            <button
+                onClick={GetGeom}
+            >
+                Get Route Graph!
+            </button>
+            <MapContainer ref={setMap} center={position} zoom={13} style={mapStyle}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
